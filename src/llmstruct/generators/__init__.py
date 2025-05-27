578b319f6e7509507c6071093a37c52abdc7608c
momai <admin@momai.dev>
2025-05-28 00:21:29 +0300

    merge stage

--- src/llmstruct/generators/__init__.py
+++ src/llmstruct/generators/__init__.py
@@ -1,6 +1,5 @@
 """Generator modules for creating LLMStruct JSON outputs from parsed code data."""
 
 from .json_generator import generate_json
-from .go_json_generator import generate_go_json
 
-__all__ = ["generate_json", "generate_go_json"]
+__all__ = ["generate_json"]
